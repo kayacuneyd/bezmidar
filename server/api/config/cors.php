4f61c71a041ff379a16a21ffb3ff11d1c9c4a75b
Cüneyt Kaya <kayacuneyd@gmail.com>
2025-11-23 16:48:03 +0100

    database conn configs V9

--- server/api/config/cors.php
+++ server/api/config/cors.php
@@ -70,11 +70,12 @@ if (
 
 if ($isAllowed && $origin) {
     header("Access-Control-Allow-Origin: {$origin}");
+    header("Access-Control-Allow-Credentials: true");
     header("Vary: Origin");
 }
 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json; charset=UTF-8");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
